10b5c82961caf391b1ff966ae848f9458207411f
emontes <emontes@dstr.net>
2024-08-16 16:59:03 -0500

    useEffect en root-wrapper

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React, { useState, useEffect } from 'react';
 import './src/assets/themes/global.css'; // para Tailwind
 import { ThemeProvider } from 'styled-components';
 
@@ -12,15 +12,27 @@ const themeMap = {
   yucatan: YucatanTheme,
 };
 
-const estadoSlug = process.env.ESTADO_SLUG
+// Nuevo componente de React
+const ThemedWrapper = ({ children }) => {
+  const [currentTheme, setCurrentTheme] = useState(ChiapasTheme);
+  const [isClient, setIsClient] = useState(false);
 
-export const wrapRootElement = ({ element }) => {
-  const theme  = themeMap[estadoSlug] || ChiapasTheme;
+  useEffect(() => {
+    setIsClient(true);
+    const estadoSlug = process.env.GATSBY_ESTADO_SLUG;
+    setCurrentTheme(themeMap[estadoSlug] || ChiapasTheme);
+  }, []);
+
+  if (!isClient) return null;
+
   return (
-
-      <ThemeProvider theme={theme}>
-        {element}
-      </ThemeProvider>
-
+    <ThemeProvider theme={currentTheme}>
+      {children}
+    </ThemeProvider>
   );
+};
+
+// Modificamos wrapRootElement para usar el nuevo componente
+export const wrapRootElement = ({ element }) => {
+  return <ThemedWrapper>{element}</ThemedWrapper>;
 };
